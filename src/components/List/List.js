cc76f2c3ab50f3f67af1626e1e4338bd998b797d
Anna Kijania <kijania.anna@gmail.com>
2020-08-24 16:46:46 +0200

    Add Column.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero.js'
+import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        src: PropTypes.string,
+        children: PropTypes.node,
+      }
+      static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+      }
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} src={this.props.src} />
+        <div className={styles.description}>
+            {this.props.children}
+        </div>
+        <div className={styles.columns}>
+            <Column title='Animals'/>
+            <Column title='Plants'/>
+            <Column title='Minerals'/>
+        </div>
       </section>
     )
   }
