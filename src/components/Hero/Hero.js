c05138584ff5c2ffeb8eb997cee1cf1010b2f7e3
Anna Kijania <kijania.anna@gmail.com>
2020-08-25 10:13:00 +0200

    Format documents

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import styles from './Hero.scss';
-import PropTypes from 'prop-types';
+import React from "react";
+import styles from "./Hero.scss";
+import PropTypes from "prop-types";
 
-const Hero  = props => (
-      <header className={styles.component}>
-        <h2 className={styles.title}>{props.titleText}</h2>
-        <img className={styles.image} src={props.src} />
-      </header>
-   );
-   Hero.propTypes = {
-    titleText: PropTypes.node.isRequired,
-    src: PropTypes.string,
-  };
+const Hero = props => (
+  <header className={styles.component}>
+    <h2 className={styles.title}>{props.titleText}</h2>
+    <img className={styles.image} src={props.src} />
+  </header>
+);
+Hero.propTypes = {
+  titleText: PropTypes.node.isRequired,
+  src: PropTypes.string
+};
 export default Hero;
