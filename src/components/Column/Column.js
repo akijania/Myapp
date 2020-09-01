3438fd487acfc0da563d0665f2942b04dc24ab4e
Anna Kijania <kijania.anna@gmail.com>
2020-09-01 12:43:33 +0200

    Add functon addColumns

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 // import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
+import { settings } from '../../data/dataStore';
 
 class Column extends React.Component {
   static propTypes = {
@@ -11,6 +12,9 @@ class Column extends React.Component {
     icon: PropTypes.node,
     cards: PropTypes.array,
   };
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  }
   render() {
     const { title, icon, cards } = this.props;
     return (
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import Column from '../Column/ColumnContainer.js';
 import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
   static propTypes = {
@@ -13,12 +13,13 @@ class List extends React.Component {
     description: PropTypes.node,
     columns: PropTypes.array,
     image: PropTypes.node,
+    addColumn: PropTypes.func,
   };
   static defaultProps = {
     description: settings.defaultListDescription,
   };
   render() {
-    const { title, image, description, columns } = this.props;
+    const { title, image, description, columns, addColumn } = this.props;
     return (
       <section className={styles.component}>
         <Hero titleText={title} image={image} />
@@ -29,12 +30,12 @@ class List extends React.Component {
             <Column key={columnData.id} {...columnData} />
           ))} 
         </div>
-        {/* <div className={styles.creator}>
+        <div className={styles.creator}>
           <Creator
             text={settings.columnCreatorText}
-            action={title => this.addColumn(title)}
+            action={addColumn}
           />
-        </div> */}
+        </div>
       </section>
     );
   }
